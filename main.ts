8da7bef7e3c2c414dfd829415ab9a21f45f8c13e
pc20211140 <115052204+pc20211140@users.noreply.github.com>
2022-10-05 09:38:21 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+let count = 0
+input.onButtonPressed(Button.A, function () {
+    count = 9
+    for (let index = 0; index < 10; index++) {
+        basic.showNumber(count)
+        basic.pause(1000)
+        count += -1
+    }
+})
 basic.forever(function () {
 	
 })
